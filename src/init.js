38dd5fe5d6c73061d44e74b5bf2714f9be6e0cae
Henning Klasén <henning@hform.se>
2014-12-03 13:18:08 +0100

    Update title of rates.

--- src/init.js
+++ src/init.js
@@ -16,42 +16,42 @@
    */
   CarCalc.Data.rates = [
     {
-      title: "Klass 1",
+      title: "Prisklass 1",
       description: "Liten bil, t ex VW Up!",
       hourly: 15.20,
       daily: 182.40,
       kilometer: 2.16
     },
     {
-      title: "Klass 2",
+      title: "Prisklass 2",
       description: "Något större bil, t ex Toyota Yaris",
       hourly: 16,
       daily: 192,
       kilometer: 2.24
     },
     {
-      title: "Klass 3",
+      title: "Prisklass 3",
       description: "Stor bil, t ex Skoda Octavia",
       hourly: 16.80,
       daily: 201.60,
       kilometer: 2.40
     },
     {
-      title: "Klass 4",
+      title: "Prisklass 4",
       description: "Miljöbil, Toyota Prius",
       hourly: 17.60,
       daily: 211.20,
       kilometer: 2.48
     },
     {
-      title: "Klass 5",
+      title: "Prisklass 5",
       description: "Minibuss, VW Caddy 7-sitsig",
       hourly: 18.40,
       daily: 220.80,
       kilometer: 2.56
     },
     {
-      title: "Klass 6",
+      title: "Prisklass 6",
       description: "Skåpbil, VW Caddy fordonsgas",
       hourly: 20,
       daily: 240,
